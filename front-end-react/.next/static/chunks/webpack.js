fc35eb17e395da41bc57eedd892d7eb0568555de
marcelo-18a <marceloyogo37@gmail.com>
2025-11-04 14:52:04 -0300

    Feat:arrumando o front

--- front-end-react/.next/static/chunks/webpack.js
+++ front-end-react/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2e934fce005a9863"; }
+/******/ 		__webpack_require__.h = function() { return "49e3649080367368"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
